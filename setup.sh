98dc4b3bf132e52a9b655db8cc9891978a736dfb
Mark Holcomb <mholcomb@augusta.edu>
2022-06-09 11:37:56 -0400

    updated script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ sudo apt update && sudo apt -y upgrade
 sudo apt-get install -y libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqt5gui5 libqt5test5
 pip install opencv-python
 ifconfig | grep inet > ifconf.txt
-wget http://old.piborg.org/downloads/thunderborg/Thunderborg3.py.txt && cp ~/Pyborg-Setup/Thunderborg3.py.txt ~/Desktop/ThunderBorg.py
+wget http://old.piborg.org/downloads/thunderborg/Thunderborg3.py.txt 
+cp ~/Pyborg-Setup/Thunderborg3.py.txt ~/Desktop/ThunderBorg.py
 
 echo "static domain_name_servers=8.8.8.8" >> /etc/dhcpcd.conf
 sudo service dhcpcd restart
